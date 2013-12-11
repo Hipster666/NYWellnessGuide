d6d8262a6594e983829cdb09960aa1ec8902ed3f
Gene <ultratonic@gmail.com>
2013-12-11 12:55:49 -0500

    initial commit

---
+++ share/index.php
@@ -0,0 +1,262 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Please support NY Wellness Guide with just few clicks</title>
+    <meta name="description" content="Please share this page on your favorite social network and help us grow">
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="js/modernizr.js"></script>
+    
+    
+    <!-- for Google -->
+<meta name="description" content="Share NY Wellness Guide" />
+<meta name="keywords" content="Please support NY Wellness Guide with just few clicks" />
+
+<meta name="author" content="" />
+<meta name="copyright" content="" />
+<meta name="application-name" content="" />
+
+<!-- for Facebook -->          
+<meta property="og:title" content="Please support NY Wellness Guide with just few clicks" />
+<meta property="og:type" content="article" />
+<meta property="og:image" content="http://www.nywellnessguide.com/newsletter/new/images/logo-nywellness.gif" />
+<meta property="og:image:width" content="75" />
+<meta property="og:image:height" content="53" />
+<meta property="og:url" content="http://bit.ly/wellnyc" />
+<meta property="og:description" content="Please Check Out NY Wellness Guide for cool health links & posts " />
+
+
+    
+    
+    <div class="row">
+      <div class="large-12" style="text-align:center;">
+      
+     <a href="http://www.nywellnessguide.com"><img src="http://i.imgur.com/3Q3rGqa.gif" alt="ny wellness guide" style="display:block; border:0;"></a>
+      
+        <h1 style="font-size:2em;font-weight:normal; margin-top:1.5em; color:#36F;">Please support NY Wellness Guide with just few clicks</h1>
+      </div>
+    </div>
+    
+    
+
+    
+    
+    
+    
+        
+        <!-- Grid Example -->
+
+        <div class="row">
+          <div class="large-12 columns">
+            <div class="callout panel">
+            
+            
+              <p>Hi, thank you for stopping by! Since you already here we would absolutely love if you could help up spread the word about our website. We put a lot of effort into making it useful and interesting for our readers and it would be great if you can help us grow even more. Please share, tweet, like, link or subscribe and let your friends and family know about cool content we have to offer. Thank you.</p>
+            </div>
+          </div>
+        </div>
+        
+        
+      <!-- 4 x 3 -->
+        
+        <div class="row">
+        
+        
+          <!-- facebook -->
+                    
+          <div class="large-2 medium-2 small-4 columns">
+            <div class="callout panel">
+             <h3>Facebook</h3>
+              <p>
+              
+               <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=223475367707541";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-share-button" data-href="http://www.facebook.com/nywellnessguide" data-type="box_count"></div>
+              
+              </p>
+            </div>
+          </div>
+          
+          <!-- twitter -->
+          
+          
+          <div class="large-2 medium-2 small-4 columns">
+            <div class="callout panel social-media">
+            <h3>Twitter</h3>
+
+            <div style="margin-top:60px;"></div>
+             
+              <p>
+   
+<div>
+   <a href="https://twitter.com/nywellnessguide" class="twitter-follow-button" data-show-count="true" 
+data-show-screen-name="false" data-show-count="vertical" data-dnt="true">Follow @User</a>
+
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
+{js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";
+fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
+
+
+</p>
+            </div>
+          </div>
+          
+          <!-- LinkedIn-->
+          
+          <div class="large-2 medium-2 small-4 columns">
+            <div class="callout panel">
+            <h3>LinkedIn</h3>
+             <p style="margin-top:2px;"></p>
+              <p>    <script src="//platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
+<script type="IN/FollowCompany" data-id="2243837" data-counter="top"></script></p>
+            </div>
+          </div>
+          
+          <!-- Google+ -->
+          
+          <div class="large-2 medium-2 small-4 columns">
+            <div class="callout panel">
+            <h3>Google+</h3>
+              <p>  <!-- Place this tag where you want the widget to render. -->
+<div class="g-follow" data-annotation="vertical-bubble" data-height="20" data-href="//plus.google.com/104764323057243340695" data-rel="publisher"></div>
+
+<!-- Place this tag after the last widget tag. -->
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script></p>
+            </div>
+          </div>
+          
+          
+            <!-- Pinterest -->
+          
+          <div class="large-2 medium-2 small-4 columns">
+            <div class="callout panel">
+            
+            
+            <h3>Pinterest</h3>
+              <p style="clear:both; margin-top:20px;">
+              
+              <p style="margin:5px 0;">&nbsp;</p>
+             
+              
+<a href="//www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.nywellnessguide.com&media=http%3A%2F%2Fi.imgur.com%2FsZVFD1k.jpg&description=Cool%20Site:%20NY%20Wellness%20Guide%3A%20Yoga%2C%20Fitness%2C%20Stress%20Management%2C%20Nutrition%20and%20Well-being." data-pin-do="buttonPin" data-pin-config="above" data-pin-height="28"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_28.png" /></a>
+
+              
+              <script type="text/javascript" async src="//assets.pinterest.com/js/pinit.js"></script>
+              
+              
+              </p>
+            </div>
+          </div>
+          
+          
+          
+            <!--Twitter Counter -->
+          
+          <div class="large-2 medium-2 small-4 columns">
+            <div class="callout panel">
+            <h3>Counter</h3>
+              <p>
+              
+             <script type="text/javascript" src="http://twittercounter.com/embed/?username=NYWellnessGuide&style=bird"></script><noscript><a href="http://twittercounter.com/NYWellnessGuide">NYWellnessGuide on Twitter Counter</a></noscript>
+              
+              </p>
+            </div>
+          </div>
+          
+        
+          
+        </div>
+        
+        <!-- 6 x 2 -->
+        
+        <div class="row">
+          <div class="large-6 medium-6 columns">
+            <div class="callout panel">
+              <p>Or Join Our Weekly Newsletter with lots of great links, health posts and other very cool content............ <a href="http://eepurl.com/x3KLP">Sign Up</a></p>
+              
+              <div style="width:95%; background-color:#fff; padding:20px; text-align:left;">
+              
+             <h3>Daily Health Tip:</h3>
+              
+              <p style="margin-top:20px;"><strong>Sniff Rosemary</strong></p>
+              <img src="http://i.imgur.com/eIt8XkF.jpg">
+              <p>According to some research, catching a whiff of this aromatic herb may increase alertness and improve memory. To stay sharp, try smelling fresh rosemary or inhaling the scent of rosemary essential oil before a test or meeting.</p>
+              
+              
+              </div>
+              
+              
+              
+              
+            </div>
+          </div>
+          <div class="large-6 medium-6 columns">
+            <div class="callout panel">
+              <p style="font-size:.8em; margin-left:30px;">&nbsp;&nbsp;<a href="http://bit.ly/IHJ7Fi" target="_blank">:: Check out our latest newsletter</a></p>
+              <a href="http://bit.ly/IHJ7Fi" target="_blank"><img src="http://i.imgur.com/XeTx3iB.jpg" border="0"></a>
+            </div>
+          </div>
+        </div><!DOCTYPE html>
+        <html>
+        <head>
+          <title></title>
+        </head>
+        <body>
+        
+        </body>
+        </html>
+         <div class="row footer-row">
+				    <div class="large-8 medium-8 columns">
+				      <a href="http://www.nywellnessguide.com"><img src="http://www.nywellnessguide.com/newsletter/new/images/logo-nywellness.gif" width="75" height="53" style="border:0; display:block; margin-right:60%"></a>
+				    </div>
+				    
+                    <div class="large-4 medium-4 columns">
+				      <div class="cool-footer">
+                      
+                          <a href="http://www.facebook.com/NYWellnessGuide" target="_blank"><img src="http://www.nywellnessguide.com/newsletter/new/images/icon-large-facebook.gif" width="52" height="52" style="border:0; display:block;"></a>
+                          <a href="http://twitter.com/NYWellnessGuide" target="_blank"><img src="http://www.nywellnessguide.com/newsletter/new/images/icon-large-twitter.gif" width="52" height="52"  style="border:0; display:block;"></a>
+                          <a href="http://www.linkedin.com/company/ny-wellness-guide" target="_blank"><img src="http://www.nywellnessguide.com/newsletter/new/images/icon-large-linkedin.gif" width="52" height="52"  style="border:0; display:block;"></a>
+                          <a href="https://plus.google.com/+Nywellnessguide/" target="_blank"><img src="http://www.nywellnessguide.com/newsletter/new/images/icon-large-google.gif" width="52" height="52"  style="border:0; display:block;"></a>
+                          <a href="http://www.pinterest.com/nywellnessguide" target="_blank"><img src="http://www.nywellnessguide.com/newsletter/new/images/icon-large-pinterest.gif" width="52" height="52" ></a>
+				    </div>
+                       </div>
+          </div>
+          
+         
+         
+         <div class="row footer-row">
+            <div class="large-12 medium-12">
+        
+          <p style="background-color:#479fe1; font-family:Arial, Helvetica, sans-serif; font-size:10px; color:#fff; margin:0; padding:20px; float:right;">New York / Copyright &copy;2013. All rights reserved.  <a href="http://www.nywellnessguide.com" style="text-decoration:none; color:#fff; font-weight:bold;">NY Wellness Guide.</a><a href="http://www.bigapplemedia.com"  style="color:#fff; font-weight:bold;">Big Apple Media Group. Inc.</a></p>
+          
+          </div>
+          </div>
+          
+                   
+    
+      
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
